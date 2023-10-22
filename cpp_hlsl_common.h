b8449d39c3d0deeb4d2117e15dcf8164360f51f2
Michal Ziulek <michalziulek@gmail.com>
2023-10-22 18:27:41 +0200

    Progress.

--- cpp_hlsl_common.h
+++ cpp_hlsl_common.h
@@ -4,9 +4,9 @@
 #define float4x4 XMFLOAT4X4
 #endif
 
-#define RDH_FRAME_STATE 1
-#define RDH_VERTEX_BUFFER_STATIC 2
-#define RDH_OBJECTS_DYNAMIC 3
+static const int RDH_FRAME_STATE = 1;
+static const int RDH_VERTEX_BUFFER_STATIC = 2;
+static const int RDH_OBJECTS_DYNAMIC = 3;
 
 struct CppHlsl_Vertex {
     float x, y;
